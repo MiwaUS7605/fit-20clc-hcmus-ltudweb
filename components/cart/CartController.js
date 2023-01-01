401e22535096dd6e1255ed3c05f03821d59bf935
LHKN <79393909+LHKN@users.noreply.github.com>
2023-01-02 00:04:40 +0700

    fix: update add to cart buttons

--- components/cart/CartController.js
+++ components/cart/CartController.js
@@ -1,14 +1,20 @@
 const laundryService = require('./LaundryService');
 const createError = require('http-errors');
 const qs = require('qs');
+const db = require('../../db');
 
 class CartController {    
     async addToCart(req, res, next) {
-        
-        console.log("alkfsnlwknfwe");
         const id = req.body.idservice;
-        console.log(req.body);
+        const service = await laundryService.get(id);
         res.json({});
+
+        //let {res.locals.user.id, id} = req.body;
+
+    }
+    
+    async displayCart(req, res, next) {
+        
     }
 }
 
